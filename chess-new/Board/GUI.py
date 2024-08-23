5c050213fafe3889084c580c8319dee23f75d381
ManosFps1pt <manos.dragasakis@gmail.com>
2024-08-23 22:25:23 +0300

    board-gui
    board gui's code is cleanner and runnable. The only issue is that pygame crashes when I try to access pygame.font.SysFont() method. The code can rum because the lines of code that contain this method are commented

--- chess-new/Board/GUI.py
+++ chess-new/Board/GUI.py
@@ -9,7 +9,7 @@ class GUI:
         self.outline_color = outline_color
         self.outline_thickness = outline_thickness
         self.pieces_offset = pieces_offset
-        self.rect_size: tuple[int, int] = self.size[0] // 8, self.size[1] // 8
+        self.square_size: tuple[int, int] = self.size[0] // 8, self.size[1] // 8
         self.index_row_dict: dict = {
             0: "A",
             1: "B",
@@ -56,11 +56,13 @@ class GUI:
 
         # color a (white)
         pygame.draw.rect(
-            self.screen, self.colors[0],
+            self.screen,
+            self.colors[0],
             pygame.Rect(
                 self.top_left[0],
                 self.top_left[1],
-                self.size[0], self.size[1]
+                self.size[0],
+                self.size[1]
             )
         )
 
@@ -70,21 +72,50 @@ class GUI:
             row = (i % 4) * 2
             if colum % 2 == 0:
                 row += 1
-            x = (row * self.size[0]) + self.top_left[0]
-            y = (colum * self.size[1]) + self.top_left[1]
-            pygame.draw.rect(self.screen, self.colors[1], pygame.Rect(x, y, self.size[0], self.size[1]))
+            x = (row * self.square_size[0]) + self.top_left[0]
+            y = (colum * self.square_size[1]) + self.top_left[1]
+            pygame.draw.rect(self.screen, self.colors[1], pygame.Rect(x, y, self.square_size[0], self.square_size[1]))
 
-            """draw letters and numbers"""
-        for i in range(8):
-            letters_row = self.key_from_value(self.letters, i)
-            font = pygame.font.SysFont('Comic Sans MS', 30)
-            text_surface = font.render(letters_row, False, "#ffffff")
-            pos = self.shelltopos(f"{letters_row}1", addy=-self.rectsize[1])
-            self.screen.blit(text_surface, pos)
 
-        for i in range(8):
-            font = pygame.font.SysFont("Comic Sans MS", 30)
-            text_surface = font.render(str(abs(8 - i)), False, "#ffffff")
-            pos = self.shelltopos(f"a{i}", addx=-self.rectsize[0], addy=self.rectsize[1])
-            screen.blit(text_surface, pos)
+        # draw letters and numbers
+        #for i in range(8):
+        #    row_letter = self.index_to_row(i)
+        #    font = pygame.font.SysFont('Comic Sans MS', 30)
+        #    text_surface = font.render(row_letter, True, "#ffffff")
+        #    pos = self.top_left[0] + (i * self.square_size[0]) + 50, self.top_left[1] + self.size[1] + 50
+        #    self.screen.blit(text_surface, pos)
+
+        #for i in range(8):
+        #    font = pygame.font.SysFont("Comic Sans MS", 30)
+        #    text_surface = font.render(str(self.invert(i)), True, "#ffffff")
+        #    pos = self.top_left[0] - self.square_size[0], (self.invert(i) * self.square_size[1]) - self.square_size[1]
+        #    self.screen.blit(text_surface, pos)
+
+    @staticmethod
+    def invert(i: int):
+        return abs(8 - i)
+
+
+    def square_to_pos(self, square_pos: tuple[int, int], white_to_play: bool):
+        if white_to_play:
+            row: int = square_pos[0]
+            colum: int = square_pos[1]
+        else:
+            row: int = self.invert(square_pos[0])
+            colum: int = self.invert(square_pos[1])
+        return self.top_left[0] + (self.square_size[0] * row), self.top_left[1] + (self.square_size[1] * colum)
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((1_000, 1_000))
+    gui = GUI(screen, (800, 800), ("#ffffff", "#333333"), (100, 100), "#0000ff", 5, 5)
+    run = True
+    while run:
+        screen.fill("#000000")
+        gui.draw_board()
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("quit")
+                run = False
+
 
